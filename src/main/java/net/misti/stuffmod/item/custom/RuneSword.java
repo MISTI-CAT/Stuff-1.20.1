022e3c70eb9b5d2bd2d656b89e9f9e6e34b7b62d
MISTI-CAT <yanaisegal200@gmail.com>
2024-07-05 22:42:27 +0300

    changed the cooldown to 10 sec

--- src/main/java/net/misti/stuffmod/item/custom/RuneSword.java
+++ src/main/java/net/misti/stuffmod/item/custom/RuneSword.java
@@ -20,7 +20,7 @@ public class RuneSword extends SwordItem {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack itemStack = user.getStackInHand(hand);
         world.playSound((PlayerEntity)null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_ENDER_PEARL_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F));
-        user.getItemCooldownManager().set(this, 20);
+        user.getItemCooldownManager().set(this, 200);
         if (!world.isClient) {
             EnderPearlEntity enderPearlEntity = new EnderPearlEntity(world, user);
             enderPearlEntity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1.5F, 1.0F);
@@ -35,7 +35,7 @@ public class RuneSword extends SwordItem {
     }
 
     public RuneSword() {
-        super(ModToolMaterial.RUNE, 4, 5, new FabricItemSettings().rarity(Rarity.EPIC).maxCount(1));
+        super(ModToolMaterial.RUNE, 4, -2, new FabricItemSettings().rarity(Rarity.EPIC).maxCount(1));
 
 
     }
